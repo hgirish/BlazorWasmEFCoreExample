b25e0ffe14a20b7805827bd457d6dc5733016c76
hgirish <girish@post.com>
2020-12-25 14:36:05 -0800

    ContactRepository implementation

--- ContactsApp.Repository/ContactRepository.cs
+++ ContactsApp.Repository/ContactRepository.cs
@@ -1,5 +1,7 @@
-﻿using ContactsApp.DataAccess;
+﻿
+using ContactsApp.DataAccess;
 using ContactsApp.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,58 +10,243 @@ using System.Threading.Tasks;
 
 namespace ContactsApp.Repository
 {
+    /// <summary>
+    /// Implementation of repository for <see cref="ContactContext"/>.
+    /// </summary>
     public class ContactRepository : IRepository<Contact, ContactContext>
     {
-        public ContactContext PersistedContext { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public Task<Contact> AddAsync(Contact item, ClaimsPrincipal user)
+        private readonly DbContextFactory<ContactContext> _factory;
+        private bool disposedValue;
+        /// <summary>
+        /// Creates a new instance of the <see cref="ContactRepository"/> class.
+        /// </summary>
+        /// <param name="factory">
+        /// The <see cref="DbContextFactory{ContactContext}"/>
+        /// to use.
+        /// </param>
+        public ContactRepository(DbContextFactory<ContactContext> factory)
         {
-            throw new NotImplementedException();
+            _factory = factory;
+        }
+        public ContactContext PersistedContext { get; set; }
+        /// <summary>
+        /// Adds a new <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="item">The <see cref="Contact"/> to add.</param>
+        /// <param name="user">The logged in <see cref="ClaimsPrincipal"/>.</param>
+        /// <returns>The <see cref="Contact"/> with id set.</returns>
+        public async  Task<Contact> AddAsync(Contact item, ClaimsPrincipal user)
+        {
+            await WorkInContextAsync(context =>
+            {
+                context.Contacts.Add(item);
+                return Task.CompletedTask;
+            }, user, true);
+            return item;
         }
 
+        /// <summary>
+        /// Attaches an item to the <see cref="ContactContext"/>.
+        /// </summary>
+        /// <param name="item">The instance of the <see cref="Contact"/>.</param>
         public void Attach(Contact item)
         {
-            throw new NotImplementedException();
+            if (PersistedContext == null)
+            {
+                throw new InvalidOperationException("Only valid in a unit of work.");
+            }
+            PersistedContext.Attach(item);
         }
 
-        public Task<bool> DeleteAsync(int id, ClaimsPrincipal user)
+        /// <summary>
+        /// Delete a <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="id">Id of the <see cref="Contact"/> to delete.</param>
+        /// <param name="user">The logged in <see cref="ClaimsPrincipal"/>.</param>
+        /// <returns><c>True</c> when found and deleted.</returns>
+        public async  Task<bool> DeleteAsync(int id, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            bool? result = null;
+            await WorkInContextAsync(async context =>
+            {
+                var item = await context.Contacts.SingleOrDefaultAsync(c => c.Id == id);
+                if (item == null)
+                {
+                    result = false;
+                }
+                else
+                {
+                    context.Contacts.Remove(item);
+                }
+            }, user, true);
+            if (!result.HasValue)
+            {
+                result = true;
+            }
+            return result.Value;
         }
 
+        /// <summary>
+        /// Implements the dipose pattern.
+        /// </summary>
+        /// <param name="disposing"><c>True</c> when disposing.</param>
+        public virtual  void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (PersistedContext != null)
+                    {
+                        PersistedContext.Dispose();
+                    }
+                }
+                disposedValue = true;
+            }
+        }
+        /// <summary>
+        /// Implement <see cref="IDisposable"/>
+        /// </summary>
         public void Dispose()
         {
-            throw new NotImplementedException();
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
-
+        /// <summary>
+        /// Not implemented here (see the Blazor WebAssembly client).
+        /// </summary>
+        /// <returns>The <see cref="ICollection{Contact}"/>.</returns>
         public Task<ICollection<Contact>> GetListAsync()
         {
             throw new NotImplementedException();
         }
-
-        public Task<TPropertyType> GetPropertyValueAsync<TPropertyType>(Contact item, string propertyName)
+        /// <summary>
+        /// Grabs the value of a property. Useful for shadow properties.
+        /// </summary>
+        /// <typeparam name="TPropertyType">The type of the property.</typeparam>
+        /// <param name="item">The <see cref="Contact"/> the property is on.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>The value of the property.</returns>
+        public async  Task<TPropertyType> GetPropertyValueAsync<TPropertyType>(Contact item, string propertyName)
         {
-            throw new NotImplementedException();
+            TPropertyType value = default;
+            await WorkInContextAsync(context =>
+            {
+                value = context.Entry(item)
+                .Property<TPropertyType>(propertyName).CurrentValue;
+                return Task.CompletedTask;
+            }, null);
+            return value;
+        }
+        /// <summary>
+        /// Load a <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="id">The id of the <see cref="Contact"/> to load.</param>
+        /// <param name="user">The logged in <see cref="ClaimsPrincipal"/>.</param>
+        /// <param name="forUpdate"><c>True</c> to keep tracking on.</param>
+        /// <returns>The <see cref="Contact"/>.</returns>
+        public async Task<Contact> LoadAsync(int id, ClaimsPrincipal user, bool forUpdate = false)
+        {
+            Contact contact = null;
+            await WorkInContextAsync(async context =>
+            {
+                var contactRef = context.Contacts;
+                if (forUpdate)
+                {
+                    contactRef.AsNoTracking();
+                }
+                contact = await contactRef.SingleOrDefaultAsync(c => c.Id == id);
+            }, user);
+            return contact;
+        }
+        /// <summary>
+        /// Query the <see cref="Contact"/> database.
+        /// </summary>
+        /// <param name="query">
+        /// A delegate that provides an <see cref="IQueryable{Contact}"/>
+        /// to build on.
+        /// </param>
+        /// <returns>A <see cref="Task"/></returns>
+        public async Task QueryAsync(Func<IQueryable<Contact>, Task> query)
+        {
+            await WorkInContextAsync(async context =>
+            {
+                await query(context.Contacts.AsNoTracking().AsQueryable());
+            }, null);
+        }
+        /// <summary>
+        /// Sets original value. This is useful to check concurrency if you have
+        /// disconnected entities and are re-attaching to update.
+        /// </summary>
+        /// <typeparam name="TPropertyType">The type of the property.</typeparam>
+        /// <param name="item">The <see cref="Contact"/> being tracked.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="value">The value to set.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        public async  Task SetOriginalValueForConcurrencyAsync<TPropertyType>(Contact item, string propertyName, TPropertyType value)
+        {
+            await WorkInContextAsync(context =>
+            {
+                var tracked = context.Entry(item);
+                // we tell EF Core what version we loaded
+                tracked.Property<TPropertyType>(propertyName).OriginalValue = value;
+                // We tell EF Core to modify entity
+                tracked.State = EntityState.Modified;
+                return Task.CompletedTask;
+            },null);
+        }
+        /// <summary>
+        /// Update the <see cref="Contact"/> (without a unit of work).
+        /// </summary>
+        /// <param name="item">The <see cref="Contact"/> to update.</param>
+        /// <param name="user">The logged in <see cref="ClaimsPrincipal"/>.</param>
+        /// <returns>The updated <see cref="Contact"/>.</returns>
+        public async  Task<Contact> UpdateAsync(Contact item, ClaimsPrincipal user)
+        {
+            await WorkInContextAsync(context =>
+            {
+                context.Contacts.Attach(item);
+                return Task.CompletedTask;
+            }, user, true);
+            return item;
         }
 
-        public Task<Contact> LoadAsync(int id, ClaimsPrincipal user, bool forUpdate = false)
+        /// <summary>
+        /// Performs some work, either using the peristed context or
+        /// by generating a new context for the operation.
+        /// </summary>
+        /// <param name="work">The work to perform (passed a <see cref="ContactContext"/>).</param>
+        /// <param name="user">The current <see cref="ClaimsPrincipal"/>.</param>
+        /// <param name="saveChanges"><c>True</c> to save changes when done.</param>
+        /// <returns></returns>
+        private async Task WorkInContextAsync(
+            Func<ContactContext, Task> work,
+            ClaimsPrincipal user,
+            bool saveChanges = false)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task QueryAsync(Func<IQueryable<Contact>, Task> query)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetOriginalValueForConcurrencyAsync<TPropertyType>(Contact item, string propertyName, TPropertyType value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Contact> UpdateAsync(Contact item, ClaimsPrincipal user)
-        {
-            throw new NotImplementedException();
+            if (PersistedContext != null)
+            {
+                if (user != null)
+                {
+                    PersistedContext.User = user;
+                }
+                // do some work, save changes flag is ignored because
+                // this will be commited later
+                await work(PersistedContext);
+            }
+            else
+            {
+                using (var context = _factory.CreateDbContext())
+                {
+                    context.User = user;
+                    await work(context);
+                    if (saveChanges)
+                    {
+                        await context.SaveChangesAsync();
+                    }
+                }
+            }
         }
     }
 }
