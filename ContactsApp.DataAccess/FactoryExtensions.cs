9322459a4db2adee225164b9407813835cb647a3
Girish <girish@post.com>
2020-12-25 13:07:15 -0800

    DataAccess SeedContacts

---
+++ ContactsApp.DataAccess/FactoryExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ContactsApp.DataAccess
+{
+    public static class FactoryExtensions
+    {
+        public static IServiceCollection AddDbContextFactory<TContext>(
+            this IServiceCollection collection,
+            Action<DbContextOptionsBuilder> optionsAction = null,
+            ServiceLifetime contextAndOptionsLifetime = ServiceLifetime.Singleton)
+            where TContext : DbContext
+        {
+            collection.Add(new ServiceDescriptor(
+                typeof(DbContextOptions<TContext>),
+                sp => GetOptions<TContext>(optionsAction, sp),
+                contextAndOptionsLifetime));
+
+            return collection;
+        }
+
+        private static DbContextOptions<TContext> GetOptions<TContext>(
+            Action<DbContextOptionsBuilder> action, 
+            IServiceProvider sp = null) where TContext : DbContext
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            if (sp != null)
+            {
+                optionsBuilder.UseApplicationServiceProvider(sp);
+            }
+
+            action?.Invoke(optionsBuilder);
+            return optionsBuilder.Options;
+        }
+    }
+}
